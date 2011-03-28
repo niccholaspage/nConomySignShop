92f218c9be4151bc3cbac4d850ad0e49c0633cd1
niccholaspage <niccholaspage@gmail.com>
2011-03-27 23:01:13 -0400

    player variable instead of event.getPlayer()

--- src/com/niccholaspage/nConomySignShop/blockListener.java
+++ src/com/niccholaspage/nConomySignShop/blockListener.java
@@ -3,6 +3,7 @@ package com.niccholaspage.nConomySignShop;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.block.Sign;
+import org.bukkit.entity.Player;
 import org.bukkit.event.block.BlockListener;
 import org.bukkit.event.block.BlockRightClickEvent;
 import org.bukkit.inventory.ItemStack;
@@ -16,6 +17,7 @@ public class blockListener extends BlockListener {
 	public void onBlockRightClick(BlockRightClickEvent event){
 		if ((event.getBlock().getType().equals(Material.SIGN_POST)) || (event.getBlock().getType().equals(Material.WALL_SIGN))){
 			Sign sign = (Sign) event.getBlock().getState();
+			Player player = event.getPlayer();
 			if (!(sign.getLine(0).equalsIgnoreCase("[nConomy]"))) return;
 			if ((!(plugin.isInt(sign.getLine(1)))) || (!(plugin.isInt(sign.getLine(2))) || (!(plugin.isInt(sign.getLine(3)))))) return;
 			Integer ID = Integer.parseInt(sign.getLine(1));
@@ -24,14 +26,14 @@ public class blockListener extends BlockListener {
 			if (plugin.econHandler.canAddorDelete(pay) == false) return;
 			if (Material.getMaterial(ID) == null) return;
 			if (amount == 0) return;
-			if (plugin.econHandler.getMoney(event.getPlayer()) < pay) return;
+			if (plugin.econHandler.getMoney(player) < pay) return;
 			ItemStack item = new ItemStack(0);
 			item.setTypeId(ID);
 			item.setAmount(amount);
-			event.getPlayer().getInventory().addItem(item);
-			event.getPlayer().updateInventory();
+			player.getInventory().addItem(item);
+			player.updateInventory();
 			plugin.econHandler.removeMoney(event.getPlayer(), pay);
-			event.getPlayer().sendMessage(ChatColor.BLUE + "You just bought " + amount + " " + Material.getMaterial(ID).name().toLowerCase().replace("_", " ") + " for " + pay + " " + plugin.econHandler.currencyName + ".");
+			player.sendMessage(ChatColor.BLUE + "You just bought " + amount + " " + Material.getMaterial(ID).name().toLowerCase().replace("_", " ") + " for " + pay + " " + plugin.econHandler.currencyName + ".");
 		}
 	}
 }
